abfd2909c598d3e3025aa2c705b9c0e1083d284e
JK-17 <118494774+JK-17@users.noreply.github.com>
2024-02-13 14:51:09 +0900

    Update 부족한 금액 계산하기.java

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -1,6 +1,6 @@
 class Solution {
     public long solution(int price, int money, int count) {
-        long[] sum = new long[count];
+        long[] sum = new long[count];    //형변환 int, long의 기본자료형에 대한 최댓값에 대한 인지 부족. 다음부터는 명심
         sum[0] = price;
         for(int i = 1; i < count; i++) sum[i] = price * (i+1) + sum[i-1];
         if(money < sum[count-1]) return sum[count-1] - money;
